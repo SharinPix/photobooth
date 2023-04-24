83a20512855a7617ba7abf1b66980e45b050ef1a
keshav <keshav.caleechurn@spoonconsulting.com>
2023-04-24 17:01:14 +0400

    ol wip

--- app/components/image-viewer.ts
+++ app/components/image-viewer.ts
@@ -1,12 +1,82 @@
 import Component from '@glimmer/component';
+import Photobooth from './photobooth';
+import { tracked } from '@glimmer/tracking';
+import { type Map } from 'ol';
+import { Resolved, task } from 'ember-concurrency';
+import RSVP from 'rsvp';
 
 interface ImageViewerArgs {
-    src: string;
+    photobooth: Photobooth,
+    photo: any
 }
 
 export default class ImageViewerComponent extends Component<ImageViewerArgs> {
+    @tracked ptb: Photobooth = this.args.photobooth;
+    @tracked width: number | undefined;
+    @tracked height: number | undefined;
 
-    get src(): string {
-        return this.args.src;
+    map: Map | null = null;
+    elem: HTMLElement | null = null;
+
+    get photo(): any {
+        return this.args.photo;
     }
+
+    initTask = task({ enqueue: true }, async (e: HTMLElement): Promise<void> => {
+        this.elem = e;
+        this.width = e.clientWidth;
+        this.height = e.clientHeight;
+
+        const p = RSVP.Promise.all([
+          import('ol/Map'),
+          import('ol/View'),
+          import('ol/layer'),
+          import('ol/proj/Projection'),
+          import('ol/interaction'),
+          import('ol/source'),
+          import('ol/extent'),
+        ]);
+
+        const [Map, View, Layer, Projection, Interaction, Source, Extent]: Resolved<
+          typeof p
+        > = await RSVP.Promise.resolve(p);
+
+        const extent = [0, 0, 1000, 1000];
+
+        const projection = new Projection.default({
+          code: 'sample_id',
+          units: 'pixels',
+          extent,
+        });
+
+        const view = new View.default({
+          projection,
+          center: Extent.getCenter(extent),
+          extent,
+          zoom: 1,
+          maxZoom: 10,
+          minZoom: 1,
+        });
+
+        const imageStatic = new Source.ImageStatic({
+          url: URL.createObjectURL(this.photo) || '',
+          projection,
+          imageExtent: extent,
+          crossOrigin: 'Anonymous',
+        });
+
+        const image = new Layer.Image({
+          source: imageStatic,
+        });
+
+        this.map = new Map.default({
+          target: e.querySelector('.map') as HTMLElement,
+          view,
+          controls: [],
+          interactions: Interaction.defaults().extend([
+            new Interaction.DragRotateAndZoom(),
+          ]),
+          layers: [image],
+        });
+    });
 }
--- app/components/photobooth.ts
+++ app/components/photobooth.ts
@@ -3,29 +3,32 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class Photobooth extends Component {
-    @tracked src: string = '';
-    @tracked filename: string = '';
+    @tracked photo: any | undefined;
+    // @tracked filename: string = '';
 
     @action uploadPhoto(event: Event) {
       event.preventDefault();
       let uploader = document.getElementById('photo-id') as HTMLInputElement;
       if (uploader && uploader.files && uploader.files[0]) {
-          this.src = URL.createObjectURL(uploader.files[0]);
-          this.filename = uploader.files[0].name;
+        //   this.src = URL.createObjectURL(uploader.files[0]);
+        //   this.filename = uploader.files[0].name;
+        this.photo = uploader.files[0];
       }
     }
 
     @action downloadPhoto() {
-        var link=document.createElement('a');
-        link.href = this.src;
-        link.download = this.filename;
-        link.target = '_blank';
-        link.rel ='noopener noreferrer';
-        link.click();
+        if (this.photo && this.photo.src && this.photo.filename) {
+            var link=document.createElement('a');
+            link.href = this.photo.src;
+            link.download = this.photo.filename;
+            link.target = '_blank';
+            link.rel ='noopener noreferrer';
+            link.click();
+        }
     }
 
     @action clearPhoto() {
-        this.src = '';
-        this.filename = '';
+        this.photo = undefined;
+        // this.filename = '';
     }
 }
