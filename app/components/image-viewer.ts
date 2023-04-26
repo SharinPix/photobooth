58e8c3348588416d11aab0d920c3439a79106639
keshav <keshav.caleechurn@spoonconsulting.com>
2023-04-26 15:52:32 +0400

    export and download working

--- app/components/image-viewer.ts
+++ app/components/image-viewer.ts
@@ -10,9 +10,11 @@ import Source from 'ol/source';
 import Extent from 'ol/extent';
 import { service } from '@ember/service';
 import OverlayService from 'photobooth/services/overlay';
+import Photobooth from './photobooth';
 
 interface ImageViewerArgs {
-    image: any
+    image: any,
+    photobooth: Photobooth
 }
 
 export default class ImageViewerComponent extends Component<ImageViewerArgs> {
@@ -22,14 +24,26 @@ export default class ImageViewerComponent extends Component<ImageViewerArgs> {
 
     map: Map | null = null;
     elem: HTMLElement | null = null;
+    photobooth: any =  this.args.photobooth;
 
     get image(): any {
         return this.args.image;
     }
 
+    get mapCanvas(): HTMLCanvasElement | null {
+      if (!this.map) return null;
+      const canvas = this.map
+        .getViewport()
+        .querySelector('.ol-layer canvas, canvas.ol-layer');
+      if (canvas instanceof HTMLCanvasElement) return canvas;
+      return null;
+    }
+
     initTask = task({ enqueue: true }, async (e: HTMLElement): Promise<void> => {
         this.elem = e;
-
+        this.width = e.clientWidth;
+        this.height = e.clientHeight;
+        this.photobooth.registerChild(this);
         const extent = [0, 0, this.image.width, this.image.height];
 
         const projection = new Projection({
@@ -68,4 +82,63 @@ export default class ImageViewerComponent extends Component<ImageViewerArgs> {
           layers: [image],
         });
     });
+
+    exportTask = task(
+      async (
+        scale: number
+      ): Promise<{ x: number; y: number; dataUrl: string }> => {
+        if (!this.map) throw new Error('Map was not loaded yet.');
+        const mapCanvas = document.createElement('canvas');
+        if (this.width) mapCanvas.width = (this.width * scale) as number;
+        if (this.height) mapCanvas.height = (this.height * scale) as number;
+        const mapContext = mapCanvas.getContext('2d');
+        const canvas = this.mapCanvas;
+
+        if (!(canvas instanceof HTMLCanvasElement)) {
+          throw new Error('Canvas not loaded yet.');
+        }
+
+        let matrix: null | [number, number, number, number, number, number] =
+          null;
+        const transform = canvas.style.transform;
+
+        if (canvas.width > 0) {
+          matrix = transform
+            // eslint-disable-next-line no-useless-escape
+            .match(/^matrix\(([^\(]*)\)$/)?.[1]
+            ?.split(',')
+            .map((x) => Number(x)) as [
+            number,
+            number,
+            number,
+            number,
+            number,
+            number
+          ];
+        } else {
+          matrix = [
+            parseFloat(canvas.style.width) / canvas.width,
+            0,
+            0,
+            parseFloat(canvas.style.height) / canvas.height,
+            0,
+            0,
+          ];
+        }
+
+        if (!matrix) throw new Error('Could not export (matrix)');
+        const transformMatrix = new DOMMatrix(matrix.map((i) => i * scale));
+        mapContext?.setTransform(transformMatrix);
+        mapContext?.drawImage(canvas, 0, 0);
+
+        if (!this.elem) {
+          throw new Error('Could not export');
+        }
+        return {
+          x: (this.elem.offsetLeft + this.elem.clientLeft) * scale,
+          y: (this.elem.offsetTop + this.elem.clientTop) * scale,
+          dataUrl: mapCanvas.toDataURL(),
+        };
+      }
+    );
 }
--- app/components/photobooth.ts
+++ app/components/photobooth.ts
@@ -1,10 +1,50 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import ImageViewerComponent from './image-viewer';
 
 export default class Photobooth extends Component {
     @tracked photo: any | undefined;
     @tracked image: any | undefined;
+
+    imageViewer: any;
+
+    saveTask = task({ enqueue: true }, async () => {
+      const rect = (
+        document.querySelector('.container') as Element
+      );
+      let elem = rect.getBoundingClientRect();
+      const scale = 1920 / elem.width;
+      const result = await this.imageViewer.exportTask.perform(scale);
+
+
+      const canvas = document.createElement('canvas');
+      canvas.width = 1920;
+      canvas.height = 1920 * (2 / 3);
+
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+      }
+
+      const img = new window.Image();
+      await new Promise<void>((resolve) => {
+        img.setAttribute('src', result.dataUrl);
+
+        img.addEventListener('load', () => {
+          context?.drawImage(img, result.x, result.y);
+          resolve();
+        });
+      });
+      this.downloadPhoto(img);
+    });
+
+
+    registerChild(component: ImageViewerComponent) {
+      this.imageViewer = component;
+    }
 
     @action async uploadPhoto(event: Event) {
       event.preventDefault();
@@ -30,15 +70,13 @@ export default class Photobooth extends Component {
       }
     }
 
-    @action downloadPhoto() {
-        if (this.photo && this.photo.src && this.photo.filename) {
-            var link=document.createElement('a');
-            link.href = this.photo.src;
-            link.download = this.photo.filename;
-            link.target = '_blank';
-            link.rel ='noopener noreferrer';
-            link.click();
-        }
+    @action downloadPhoto(img: any) {
+      var link=document.createElement('a');
+      link.href = img.src;
+      link.download = this.photo.name;
+      link.target = '_blank';
+      link.rel ='noopener noreferrer';
+      link.click();
     }
 
     @action clearPhoto() {
